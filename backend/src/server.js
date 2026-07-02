bbb78b0eade4d61cc43b893803eba6c9460dee04
Ismael Vinicios Oliveira da Silva <980302@queroquero.local>
2026-07-01 22:55:32 -0300

    feat: adiciona rotas de envios de taloes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const { query, pool } = require("./config/database");
 
 const { aplicarCors, enviarJson } = require("./utils/http");
 const { tratarRotasLojas } = require("./routes/lojas.routes");
+const { tratarRotasEnvios } = require("./routes/envios.routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -27,6 +28,12 @@ async function roteador(req, res) {
   const rotaLojasAtendida = await tratarRotasLojas(req, res, url);
 
   if (rotaLojasAtendida) {
+    return;
+  }
+
+  const rotaEnviosAtendida = await tratarRotasEnvios(req, res, url);
+
+  if (rotaEnviosAtendida) {
     return;
   }
 
